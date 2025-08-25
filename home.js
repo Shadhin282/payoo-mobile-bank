da594fe7b4f655ba779a5e869b9f5c3eece77a18
shadin khan <13307041@iubat.edu>
2025-08-25 12:57:27 +0600

    update

--- home.js
+++ home.js
@@ -1,15 +1,31 @@
+const validPin = 1234
+
 document.getElementById("add-money-btn").addEventListener('click', function (e) {
     e.preventDefault()
     // console.log("add money clicked.")
     const bank = document.getElementById("bank").value;
     const accountNumber = document.getElementById('account-number').value;
     const amount = parseInt(document.getElementById('add-amount').value);
-    const pin = document.getElementById('add-pin').value;
+    const pin = parseInt(document.getElementById('add-pin').value);
     const availableBalance = parseInt(document.getElementById('available-balance').innerText);
     // console.log(availableBalance)
-    const totalNewAvailableBalance = amount + availableBalance;
+
+    if (accountNumber.length < 11) {
+        alert("Invalid number. kindly provide 11 digit.")
+        return;
+    } 
+
+    if (pin !== validPin) {
+        alert("Pin not match.")
+        return;
+    }
+    
+          const totalNewAvailableBalance = amount + availableBalance;
     // console.log(totalNewAvailableBalance)
-    document.getElementById("available-balance").innerText = totalNewAvailableBalance;
+         document.getElementById("available-balance").innerText = totalNewAvailableBalance;
+    
+
+  
 
 
 })
